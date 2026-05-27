9f83f2b45aca6981a3c464e75fc3c8c4f46710a0
theamadison <alfoxthea@gmail.com>
2026-05-26 23:55:14 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,59 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  return <></>;
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  /** Shows the details of the selected episode */
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+    return (
+      <section className="details">
+        <h2>Episode {selectedEpisode.id}</h2>
+        <h3>{selectedEpisode.title}</h3>
+        <p>{selectedEpisode.description}</p>
+        <button>Watch now</button>
+      </section>
+    );
+  }
+
+  /** Shows the list of episodes available and shows more details when an episode is selected */
+  function EpisodeList() {
+    return (
+      <section className="episodes">
+        <h2>Episodes</h2>
+        <ol>
+          {episodes.map((episode) => (
+            <li
+              key={episode.id}
+              onClick={() => setSelectedEpisode(episode)}
+              className={selectedEpisode?.id === episode.id ? "selected" : ""}
+            >
+              {episode.title}
+            </li>
+          ))}
+        </ol>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <EpisodeList />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+/** StrictMode helps figure out where bugs may be located at during the early stages of development */
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
